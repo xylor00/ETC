ad685ad7ad1c941c28e6fc24a1e10150669fd090
xylor <xylor@mail.ustc.edu.cn>
2025-03-21 00:01:56 +0800

    Extract IP packet header from pcap

--- test.py
+++ test.py
@@ -1,27 +1,86 @@
-import torch
-from byol_pytorch import BYOL
-from torchvision import models
+import dpkt
 
-resnet = models.resnet50(pretrained=True)
+max_byte_len = 12
 
-learner = BYOL(
-    resnet,
-    image_size = 256,
-    hidden_layer = 'avgpool'
-)
+categories = ["Chat", "Email"]
+labels = [0, 1]
 
-opt = torch.optim.Adam(learner.parameters(), lr=3e-4)
 
-def sample_unlabelled_images():
-    return torch.randn(20, 3, 256, 256)
+def gen_pkts(pcap, label):
+    pkts = {}
 
-for _ in range(100):
-    images = sample_unlabelled_images()
-    loss = learner(images)
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
-    learner.update_moving_average() # update moving average of target encoder
+    if pcap.datalink() != dpkt.pcap.DLT_EN10MB:
+        print('unknown data link!')
+        return
 
-# save your improved network
-torch.save(resnet.state_dict(), './improved-net.pt')
+    pkts[str(label)] = []
+
+    for _, buff in pcap:
+
+        eth = dpkt.ethernet.Ethernet(buff)
+
+        if isinstance(eth.data, dpkt.ip.IP) and (
+                isinstance(eth.data.data, dpkt.udp.UDP)
+                or isinstance(eth.data.data, dpkt.tcp.TCP)):
+            ip = eth.data
+            pkts[str(label)].append(ip)
+
+    return pkts
+
+
+def closure(pkts):
+
+    pkts_dict = {}
+    for name in categories:
+        index = categories.index(name)
+        pkts_dict[name] = pkts[index]
+    
+    return pkts_dict
+
+def pkt2feature(data):
+    data_dict = []
+
+    for c in categories:
+        all_pkts = []
+        p_keys = list(data[c].keys())
+
+        for key in p_keys:
+            all_pkts = data[c][key]
+            print(len(all_pkts))
+
+
+        for idx in range(len(all_pkts)):
+            pkt = all_pkts[idx]
+            raw_byte = pkt.pack()
+
+            byte = []
+            pos = []
+            for x in range(min(len(raw_byte), max_byte_len)):
+                byte.append(int(raw_byte[x]))
+                pos.append(x)
+
+            byte.extend([0] * (max_byte_len - len(byte)))
+            pos.extend([0] * (max_byte_len - len(pos)))
+            byte.append(c)
+            
+            data_dict.append(byte)
+            
+            
+    return data_dict
+
+
+pkts_list = []
+
+chat = dpkt.pcap.Reader(open('testchat.pcap', 'rb'))
+chat_dic = gen_pkts(chat, 0)
+pkts_list.append(chat_dic)
+
+email = dpkt.pcap.Reader(open('testemail.pcap', 'rb'))
+email_dic = gen_pkts(email, 1)
+pkts_list.append(email_dic)
+
+data = closure(pkts_list)
+
+IPbytes = pkt2feature(data)
+
+print(IPbytes)
